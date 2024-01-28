f5519ba7d53610f204db61ef505e0cf2e5fbcf9f
ilyasBozdemir <bozdemir.ib70@gmail.com>
2024-01-28 17:11:21 +0300

    upd

--- components/Showcase/ShowcaseImage.jsx
+++ components/Showcase/ShowcaseImage.jsx
@@ -1,5 +1,5 @@
 import { Box } from "@chakra-ui/react";
-import Image from "next/Image";
+import Image from "next/image";
 import React, { useEffect } from "react";
 import { showcaseImages } from "../../mocks/showcaseImages";
 
